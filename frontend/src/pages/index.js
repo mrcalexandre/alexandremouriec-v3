d575e5164ecc76d3fcf0a3eb7a90782cf1166a28
Alexandre Mouriec <alexandre.mouriec@gmail.com>
2020-10-19 19:55:31 +0200

    ajout experience section

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -80,77 +80,325 @@ export default function IndexPage({ fixed }) {
               src="/icons/AlexandreMouriec.png"
               alt="Photo"
             />
-            <h2 className="text-3xl text-center tracking-tight leading-10 font-extrabold text-gray-900 sm:text-2xl sm:leading-none md:text-3xl mt-6">
+            <h1 className="sm:text-2xl text-3xl text-center tracking-tight leading-10 font-extrabold text-gray-900 sm:leading-none md:text-3xl mt-6">
               Looking for a Full-Stack/Frontend role in London 🇬🇧
-            </h2>
-            <div className="flex flex-row mt-2 items-center">
+            </h1>
+            <div className="flex flex-row justify-center mt-4">
               <a
-                href="https://github.com/remi2j"
-                title="GitHub"
+                href="https://twitter.com/mrcalexandre"
+                title="Twitter"
                 target="_blank"
                 rel="noopener noreferrer"
-                class="shadow-sm hover:shadow bg-white text-gray-900 mr-2 py-2 px-3 rounded-full"
+                className="shadow-sm hover:shadow bg-color-twitter py-3 px-3 rounded-full"
               >
                 <svg
-                  stroke="currentColor"
+                  stroke="white"
                   fill="none"
-                  stroke-width="2"
+                  strokeWidth={2}
                   viewBox="0 0 24 24"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  height="1em"
-                  width="1em"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  height="1.5em"
+                  width="1.5em"
                   xmlns="http://www.w3.org/2000/svg"
                 >
-                  <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                  <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
                 </svg>
               </a>
               <a
-                href="https://github.com/remi2j"
-                title="GitHub"
+                href="https://linkedin.com/in/alexandremouriec"
+                title="Linkedin"
                 target="_blank"
                 rel="noopener noreferrer"
-                class="shadow-sm hover:shadow bg-white text-gray-900 mr-2 py-2 px-3 rounded-full"
+                className="shadow-sm hover:shadow bg-color-linkedin mx-4 py-3 px-3 rounded-full"
               >
                 <svg
-                  stroke="currentColor"
+                  stroke="white"
                   fill="none"
-                  stroke-width="2"
+                  strokeWidth={2}
                   viewBox="0 0 24 24"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  height="1em"
-                  width="1em"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  height="1.5em"
+                  width="1.5em"
                   xmlns="http://www.w3.org/2000/svg"
                 >
-                  <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                  <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
+                  <rect x="2" y="9" width="4" height="12"></rect>
+                  <circle cx="4" cy="4" r="2"></circle>
                 </svg>
               </a>
               <a
-                href="https://github.com/remi2j"
+                href="https://github.com/mrcalexandre"
                 title="GitHub"
                 target="_blank"
                 rel="noopener noreferrer"
-                class="shadow-sm hover:shadow bg-white text-gray-900 mr-2 py-2 px-3 rounded-full"
+                className="shadow-sm hover:shadow bg-color-github py-3 px-3 rounded-full"
               >
                 <svg
-                  stroke="currentColor"
+                  stroke="white"
                   fill="none"
-                  stroke-width="2"
+                  strokeWidth={2}
                   viewBox="0 0 24 24"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  height="1em"
-                  width="1em"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  height="1.5em"
+                  width="1.5em"
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
                 </svg>
               </a>
             </div>
+            <p className="sm:text-sm sm:mx-auto mt-4 text-xl mx-auto text-center leading-relaxed text-gray-600 lg:mx-auto ">
+              Lorem ipsum dolor sit amet consect adipisicing elit. Possimus
+              magnam voluptatum cupiditate veritatis in accusamus quisquam.
+            </p>
           </div>
         </div>
       </div>
+
+      <div className="flex flex-col">
+        <h1 className="text-4xl text-center tracking-tight leading-10 font-extrabold text-gray-900 sm:text-2xl sm:leading-none md:text-4xl mt-6">
+          Experience
+        </h1>
+        <div className="my-2 overflow-x-auto sm:-mx-3 lg:-mx-4 text-center">
+          <div className="py-2 align-middle inline-block sm:px-6 lg:px-8">
+            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead>
+                  <tr>
+                    <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                      Name
+                    </th>
+                    <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                      Title
+                    </th>
+                    <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                      Status
+                    </th>
+                    <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                      Role
+                    </th>
+                    <th className="px-6 py-3 bg-gray-50"></th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200">
+                  <tr className="hover:bg-gray-100">
+                    <td className="px-6 py-4 whitespace-no-wrap">
+                      <div className="flex items-center">
+                        <div className="flex-shrink-0 h-10 w-10">
+                          <img
+                            className="h-10 w-10 rounded-full"
+                            src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60"
+                            alt=""
+                          />
+                        </div>
+                        <div className="ml-4">
+                          <div className="text-sm leading-5 font-medium text-gray-900">
+                            Jane Cooper
+                          </div>
+                          <div className="text-sm leading-5 text-gray-500">
+                            jane.cooper@example.com
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-no-wrap">
+                      <div className="text-sm leading-5 text-gray-900">
+                        Regional Paradigm Technician
+                      </div>
+                      <div className="text-sm leading-5 text-gray-500">
+                        Optimization
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-no-wrap">
+                      <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                        Active
+                      </span>
+                    </td>
+                    <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
+                      Admin
+                    </td>
+                    <td className="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
+                      <a
+                        href="#"
+                        className="text-indigo-600 hover:text-indigo-900"
+                      >
+                        Edit
+                      </a>
+                    </td>
+                  </tr>
+                  <tr className="hover:bg-gray-100">
+                    <td className="px-6 py-4 whitespace-no-wrap">
+                      <div className="flex items-center">
+                        <div className="flex-shrink-0 h-10 w-10">
+                          <img
+                            className="h-10 w-10 rounded-full"
+                            src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60"
+                            alt=""
+                          />
+                        </div>
+                        <div className="ml-4">
+                          <div className="text-sm leading-5 font-medium text-gray-900">
+                            Jane Cooper
+                          </div>
+                          <div className="text-sm leading-5 text-gray-500">
+                            jane.cooper@example.com
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-no-wrap">
+                      <div className="text-sm leading-5 text-gray-900">
+                        Regional Paradigm Technician
+                      </div>
+                      <div className="text-sm leading-5 text-gray-500">
+                        Optimization
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-no-wrap">
+                      <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                        Active
+                      </span>
+                    </td>
+                    <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
+                      Admin
+                    </td>
+                    <td className="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
+                      <a
+                        href="#"
+                        className="text-indigo-600 hover:text-indigo-900"
+                      >
+                        Edit
+                      </a>
+                    </td>
+                  </tr>
+                  <tr className="hover:bg-gray-100">
+                    <td className="px-6 py-4 whitespace-no-wrap">
+                      <div className="flex items-center">
+                        <div className="flex-shrink-0 h-10 w-10">
+                          <img
+                            className="h-10 w-10 rounded-full"
+                            src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60"
+                            alt=""
+                          />
+                        </div>
+                        <div className="ml-4">
+                          <div className="text-sm leading-5 font-medium text-gray-900">
+                            Jane Cooper
+                          </div>
+                          <div className="text-sm leading-5 text-gray-500">
+                            jane.cooper@example.com
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-no-wrap">
+                      <div className="text-sm leading-5 text-gray-900">
+                        Regional Paradigm Technician
+                      </div>
+                      <div className="text-sm leading-5 text-gray-500">
+                        Optimization
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-no-wrap">
+                      <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                        Active
+                      </span>
+                    </td>
+                    <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
+                      Admin
+                    </td>
+                    <td className="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
+                      <a
+                        href="#"
+                        className="text-indigo-600 hover:text-indigo-900"
+                      >
+                        Edit
+                      </a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <footer class="flex flex-col justify-between items-center px-6 py-2 bg-gray-100 sm:flex-row">
+        <a href="#" class="text-md font-bold text-gray-700 hover:text-gray-400">
+          © 2020 - Alexandre Mouriec
+        </a>
+
+        <div class="flex -mx-2 text-gray-700">
+          <a
+            href="https://twitter.com/mrcalexandre"
+            title="Twitter"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="py-3 px-3"
+          >
+            <svg
+              stroke="currentColor"
+              fill="none"
+              strokeWidth={2}
+              viewBox="0 0 24 24"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              height="1.5em"
+              width="1.5em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
+            </svg>
+          </a>
+          <a
+            href="https://linkedin.com/in/alexandremouriec"
+            title="Linkedin"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mx-2 py-3 px-3"
+          >
+            <svg
+              stroke="currentColor"
+              fill="none"
+              strokeWidth={2}
+              viewBox="0 0 24 24"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              height="1.5em"
+              width="1.5em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
+              <rect x="2" y="9" width="4" height="12"></rect>
+              <circle cx="4" cy="4" r="2"></circle>
+            </svg>
+          </a>
+          <a
+            href="https://github.com/mrcalexandre"
+            title="GitHub"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="py-3 px-3"
+          >
+            <svg
+              stroke="currentColor"
+              fill="none"
+              strokeWidth={2}
+              viewBox="0 0 24 24"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              height="1.5em"
+              width="1.5em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+            </svg>
+          </a>
+        </div>
+      </footer>
     </>
   )
 }
