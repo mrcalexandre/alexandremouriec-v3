0b8fc8ac3a07e47c5d08568a638ea8eccc6d8ab8
Alexandre Mouriec <alexandre.mouriec@gmail.com>
2020-10-20 21:18:07 +0200

    amelioration homepage

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -5,7 +5,7 @@ export default function IndexPage({ fixed }) {
   const [navbarOpen, setNavbarOpen] = React.useState(false)
   return (
     <>
-      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-gray-100 mb-3">
+      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg  mb-3">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <a
@@ -46,26 +46,26 @@ export default function IndexPage({ fixed }) {
             <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
               <li className="nav-item">
                 <a
-                  className="px-3 py-2 flex items-center text-xl font-bold text-gray-900 hover:opacity-75"
+                  className="px-3 py-2 flex items-center text-xl font-bold rounded-md text-gray-900 hover:bg-gray-200"
                   href="#about"
                 >
-                  <span className="ml-2">About</span>
+                  <span className="mx-1">About</span>
                 </a>
               </li>
               <li className="nav-item">
                 <a
-                  className="px-3 py-2 flex items-center text-xl font-bold text-gray-900 hover:opacity-75"
+                  className="px-3 py-2 flex items-center text-xl font-bold rounded-md text-gray-900 hover:bg-gray-200"
                   href="#projects"
                 >
-                  <span className="ml-2">Projects</span>
+                  <span className="mx-1">Projects</span>
                 </a>
               </li>
               <li className="nav-item">
                 <a
-                  className="px-3 py-2 flex items-center text-xl font-bold text-gray-900 hover:opacity-75"
+                  className="px-3 py-2 flex items-center text-xl font-bold rounded-md text-gray-900 hover:bg-gray-200"
                   href="#contact"
                 >
-                  <span className="ml-2">Contact</span>
+                  <span className="mx-1">Contact</span>
                 </a>
               </li>
             </ul>
@@ -89,7 +89,7 @@ export default function IndexPage({ fixed }) {
                 title="Twitter"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="shadow-sm hover:shadow bg-color-twitter py-3 px-3 rounded-full"
+                className="shadow-sm hover:shadow-xl bg-color-twitter py-3 px-3 rounded-full"
               >
                 <svg
                   stroke="white"
@@ -110,7 +110,7 @@ export default function IndexPage({ fixed }) {
                 title="Linkedin"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="shadow-sm hover:shadow bg-color-linkedin mx-4 py-3 px-3 rounded-full"
+                className="shadow-sm hover:shadow-xl bg-color-linkedin mx-4 py-3 px-3 rounded-full"
               >
                 <svg
                   stroke="white"
@@ -133,7 +133,7 @@ export default function IndexPage({ fixed }) {
                 title="GitHub"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="shadow-sm hover:shadow bg-color-github py-3 px-3 rounded-full"
+                className="shadow-sm hover:shadow-xl bg-color-github py-3 px-3 rounded-full"
               >
                 <svg
                   stroke="white"
@@ -154,12 +154,18 @@ export default function IndexPage({ fixed }) {
               Lorem ipsum dolor sit amet consect adipisicing elit. Possimus
               magnam voluptatum cupiditate veritatis in accusamus quisquam.
             </p>
+            <button
+              type="button"
+              class=" flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-green-400 hover:bg-green-600 active:bg-indigo-700 transition duration-150 ease-in-out"
+            >
+              More about me
+            </button>
           </div>
         </div>
       </div>
 
       <div className="flex flex-col">
-        <h1 className="text-4xl text-center tracking-tight leading-10 font-extrabold text-gray-900 sm:text-2xl sm:leading-none md:text-4xl mt-6">
+        <h1 className="text-4xl text-center tracking-tight leading-10 font-extrabold text-gray-900 sm:text-2xl sm:leading-none md:text-4xl mt-12">
           Experience
         </h1>
         <div className="my-2 overflow-x-auto sm:-mx-3 lg:-mx-4 text-center">
@@ -184,7 +190,7 @@ export default function IndexPage({ fixed }) {
                   </tr>
                 </thead>
                 <tbody className="bg-white divide-y divide-gray-200">
-                  <tr className="hover:bg-gray-100">
+                  <tr className="hover:bg-gray-100 hover:shadow-md">
                     <td className="px-6 py-4 whitespace-no-wrap">
                       <div className="flex items-center">
                         <div className="flex-shrink-0 h-10 w-10">
@@ -229,7 +235,7 @@ export default function IndexPage({ fixed }) {
                       </a>
                     </td>
                   </tr>
-                  <tr className="hover:bg-gray-100">
+                  <tr className="hover:bg-gray-100 hover:shadow-md">
                     <td className="px-6 py-4 whitespace-no-wrap">
                       <div className="flex items-center">
                         <div className="flex-shrink-0 h-10 w-10">
@@ -274,7 +280,7 @@ export default function IndexPage({ fixed }) {
                       </a>
                     </td>
                   </tr>
-                  <tr className="hover:bg-gray-100">
+                  <tr className="hover:bg-gray-100 hover:shadow-md">
                     <td className="px-6 py-4 whitespace-no-wrap">
                       <div className="flex items-center">
                         <div className="flex-shrink-0 h-10 w-10">
@@ -325,13 +331,263 @@ export default function IndexPage({ fixed }) {
           </div>
         </div>
       </div>
-
-      <footer class="flex flex-col justify-between items-center px-6 py-2 bg-gray-100 sm:flex-row">
-        <a href="#" class="text-md font-bold text-gray-700 hover:text-gray-400">
+      <section className="text-gray-700 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4 text-center">
+            <div className="p-4 sm:w-1/4 w-1/2 hover:shadow-xl">
+              <h2 className="title-font font-medium sm:text-4xl text-3xl text-gray-900">
+                2.7K
+              </h2>
+              <p className="leading-relaxed">Users</p>
+            </div>
+            <div className="p-4 sm:w-1/4 w-1/2 hover:shadow-xl">
+              <h2 className="title-font font-medium sm:text-4xl text-3xl text-gray-900">
+                1.8K
+              </h2>
+              <p className="leading-relaxed">Subscribes</p>
+            </div>
+            <div className="p-4 sm:w-1/4 w-1/2 hover:shadow-xl">
+              <h2 className="title-font font-medium sm:text-4xl text-3xl text-gray-900">
+                35
+              </h2>
+              <p className="leading-relaxed">Downloads</p>
+            </div>
+            <div className="p-4 sm:w-1/4 w-1/2 hover:shadow-xl">
+              <h2 className="title-font font-medium sm:text-4xl text-3xl text-gray-900">
+                4
+              </h2>
+              <p className="leading-relaxed">Products</p>
+            </div>
+          </div>
+          <div className="flex flex-wrap -m-4 text-center">
+            <div className="p-4 sm:w-1/4 w-1/2 hover:shadow-xl">
+              <h2 className="title-font font-medium sm:text-4xl text-3xl text-gray-900">
+                2.7K
+              </h2>
+              <p className="leading-relaxed">Users</p>
+            </div>
+            <div className="p-4 sm:w-1/4 w-1/2 hover:shadow-xl">
+              <h2 className="title-font font-medium sm:text-4xl text-3xl text-gray-900">
+                1.8K
+              </h2>
+              <p className="leading-relaxed">Subscribes</p>
+            </div>
+            <div className="p-4 sm:w-1/4 w-1/2 hover:shadow-xl">
+              <h2 className="title-font font-medium sm:text-4xl text-3xl text-gray-900">
+                35
+              </h2>
+              <p className="leading-relaxed">Downloads</p>
+            </div>
+            <div className="p-4 sm:w-1/4 w-1/2 hover:shadow-xl">
+              <h2 className="title-font font-medium sm:text-4xl text-3xl text-gray-900">
+                4
+              </h2>
+              <p className="leading-relaxed">Products</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="text-gray-700 body-font">
+        <h1 className="text-4xl text-center tracking-tight leading-10 font-extrabold text-gray-900 sm:text-2xl sm:leading-none md:text-4xl mt-6">
+          Experience
+        </h1>
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col text-center w-full mb-20">
+            <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">
+              ROOF PARTY POLAROID
+            </h2>
+            <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">
+              Master Cleanse Reliac Heirloom
+            </h1>
+          </div>
+          <div className="flex flex-wrap -m-4">
+            <div className="p-4 md:w-1/3">
+              <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+                <div className="flex items-center mb-3">
+                  <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      className="w-5 h-5"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                    </svg>
+                  </div>
+                  <h2 className="text-gray-900 text-lg title-font font-medium">
+                    Shooting Stars
+                  </h2>
+                </div>
+                <div className="flex-grow">
+                  <p className="leading-relaxed text-base">
+                    Blue bottle crucifix vinyl post-ironic four dollar toast
+                    vegan taxidermy. Gastropub indxgo juice poutine.
+                  </p>
+                  <a className="mt-3 text-indigo-500 inline-flex items-center">
+                    Learn More
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      className="w-4 h-4 ml-2"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M5 12h14M12 5l7 7-7 7"></path>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="p-4 md:w-1/3">
+              <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+                <div className="flex items-center mb-3">
+                  <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      className="w-5 h-5"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                      <circle cx="12" cy="7" r="4"></circle>
+                    </svg>
+                  </div>
+                  <h2 className="text-gray-900 text-lg title-font font-medium">
+                    The Catalyzer
+                  </h2>
+                </div>
+                <div className="flex-grow">
+                  <p className="leading-relaxed text-base">
+                    Blue bottle crucifix vinyl post-ironic four dollar toast
+                    vegan taxidermy. Gastropub indxgo juice poutine.
+                  </p>
+                  <a className="mt-3 text-indigo-500 inline-flex items-center">
+                    Learn More
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      className="w-4 h-4 ml-2"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M5 12h14M12 5l7 7-7 7"></path>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="p-4 md:w-1/3">
+              <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+                <div className="flex items-center mb-3">
+                  <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      className="w-5 h-5"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle cx="6" cy="6" r="3"></circle>
+                      <circle cx="6" cy="18" r="3"></circle>
+                      <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
+                    </svg>
+                  </div>
+                  <h2 className="text-gray-900 text-lg title-font font-medium">
+                    Neptune
+                  </h2>
+                </div>
+                <div className="flex-grow">
+                  <p className="leading-relaxed text-base">
+                    Blue bottle crucifix vinyl post-ironic four dollar toast
+                    vegan taxidermy. Gastropub indxgo juice poutine.
+                  </p>
+                  <a className="mt-3 text-indigo-500 inline-flex items-center">
+                    Learn More
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      className="w-4 h-4 ml-2"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M5 12h14M12 5l7 7-7 7"></path>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className="text-gray-700 body-font relative">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col text-center w-full mb-12">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+              Contact Us
+            </h1>
+            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+              Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+              gentrify.
+            </p>
+          </div>
+          <div className="lg:w-1/2 md:w-2/3 mx-auto">
+            <div className="flex flex-wrap -m-2">
+              <div className="p-2 w-1/2" data-children-count="1">
+                <input
+                  className="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2"
+                  placeholder="Name"
+                  type="text"
+                  data-kwimpalastatus="alive"
+                  data-kwimpalaid="1603132198319-0"
+                />
+              </div>
+              <div className="p-2 w-1/2" data-children-count="1">
+                <input
+                  className="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2"
+                  placeholder="Email"
+                  type="email"
+                  data-kwimpalastatus="alive"
+                  data-kwimpalaid="1603132198319-1"
+                />
+              </div>
+              <div className="p-2 w-full" data-children-count="1">
+                <textarea
+                  className="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none h-48 focus:border-indigo-500 text-base px-4 py-2 resize-none block"
+                  placeholder="Message"
+                ></textarea>
+              </div>
+              <div className="p-2 w-full">
+                <button className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                  Button
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer className="flex flex-col justify-between items-center px-6 py-2 bg-gray-100 sm:flex-row">
+        <a
+          href="#"
+          className="text-md font-bold text-gray-700 hover:text-gray-400"
+        >
           © 2020 - Alexandre Mouriec
         </a>
 
-        <div class="flex -mx-2 text-gray-700">
+        <div className="flex -mx-2 text-gray-700">
           <a
             href="https://twitter.com/mrcalexandre"
             title="Twitter"
